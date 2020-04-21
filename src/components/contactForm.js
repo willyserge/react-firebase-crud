8d71e05ef19e8fdb7c861e547aad8c2bc6594a31
willyserge <willyserge01@gmail.com>
2020-04-21 15:39:51 +0200

    initial commit

---
+++ src/components/contactForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ContactForm() {
+    return (
+        <div>
+            contact form
+        </div>
+    )
+}
+
+export default ContactForm;
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDoQhtm9cjn3Q7Xxb-jV0cxWXrQLOauIHI",
+    authDomain: "react-crud-b041b.firebaseapp.com",
+    databaseURL: "https://react-crud-b041b.firebaseio.com",
+    projectId: "react-crud-b041b",
+    storageBucket: "react-crud-b041b.appspot.com",
+    messagingSenderId: "1013025454459",
+    appId: "1:1013025454459:web:28efc348960f15647125de"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig); 
